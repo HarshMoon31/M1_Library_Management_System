00d85b4feb20026908fb33379d0aff96724e9040
HARSH MOON <98867723+HarshMoon31@users.noreply.github.com>
2022-02-06 17:45:15 +0530

    Update Search_book.c

--- 3_Implimentation/scr/Search_book.c
+++ 3_Implimentation/scr/Search_book.c
@@ -1,1 +1,19 @@
+/**
+* @file Add_book.c
+* @author Harsh Ramesh Moon
+* @brief function defination to add a new book
+* @version 0.1
+* @date 
+*
+* @copyright Copyright (c) 2021
+*
+*
+*/
 
+#include"library.h"
+
+values view_a_book(int id){
+  FILE *fptr;
+  fptr = fopen("library_record.txt","rb");
+  if(fptr==NULL){
+    printf(
